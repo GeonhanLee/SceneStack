860a12fb50c0cc03e34c815c97311d2141e09ade
GeonhanLee <leegeonhan1122@gmail.com>
2023-05-23 22:09:23 +0900

    fix where multiple bootstrapper exist, load with scenestack, not sceneStackSO

--- Assets/SampleScenes/Bootstrapper.cs
+++ Assets/SampleScenes/Bootstrapper.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Malcha.SceneStack
+namespace Malcha.SceneStack.Sample
 {
     public class Bootstrapper : MonoBehaviour
     {
-        [SerializeField] private SceneStackSO _sceneStackSO = default;
         void Start()
         {
-            SceneStackLoader.LoadSceneStack(_sceneStackSO);
+            var stack = new SceneStack("BaseScene");
+            stack.overlayScenes = new List<SceneData>
+            {
+                new SceneData("Assets/SampleScenes/OverlaySceneA.unity"),
+                new SceneData("OverlaySceneB"),
+                new SceneData("SampleScenes/OverlaySceneC")
+            };
+
+            SceneStackLoader.LoadSceneStack(stack);
         }
     }
 }
