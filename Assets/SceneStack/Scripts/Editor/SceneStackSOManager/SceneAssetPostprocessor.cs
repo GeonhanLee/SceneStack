1b74e920f24818871a64d8779c9d6bfff30b37d6
GeonhanLee <leegeonhan1122@gmail.com>
2023-05-24 19:47:44 +0900

    prevent multiple calls of ReserializeAllSceneStackSO when save multiple loaded scenes at once

--- Assets/SceneStack/Scripts/Editor/SceneStackSOManager/SceneAssetPostprocessor.cs
+++ Assets/SceneStack/Scripts/Editor/SceneStackSOManager/SceneAssetPostprocessor.cs
@@ -6,6 +6,7 @@ namespace Malcha.SceneStack.Editor
 {
     internal class SceneAssetPostprocessor : AssetPostprocessor
     {
+        private static bool _dirtyFlag = false;
         private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
         {
             bool IsScenePath(string path) => Path.GetExtension(path) == ".unity";
@@ -18,7 +19,15 @@ namespace Malcha.SceneStack.Editor
 
             if (hasSceneChange)
             {
-                EditorApplication.delayCall += () => SceneStackSOManager.ReserializeAllSceneStackSO();
+                _dirtyFlag = true;
+                EditorApplication.delayCall += () =>
+                {
+                    if (_dirtyFlag)
+                    {
+                        SceneStackSOManager.ReserializeAllSceneStackSO();
+                        _dirtyFlag = false;
+                    }
+                };
             }
         }
     }
